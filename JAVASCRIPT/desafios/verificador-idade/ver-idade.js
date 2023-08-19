1a17b0cb6c8e4a6482793514fce4675829d45422
Eliel S <hydrahtec@gmail.com>
2023-08-19 17:01:05 -0300

    Update java
    0908-1700

--- JAVASCRIPT/desafios/verificador-idade/ver-idade.js
+++ JAVASCRIPT/desafios/verificador-idade/ver-idade.js
@@ -1,4 +1,7 @@
 function idade() {
-    
+    var fnasci=window.document.getElementById('nasc')   
+    var nasci=Number(fnasci.value)
 
+    var resu=window.document.getElementById('res')
+    resu.innerText=`Seu ano de nascimento é ${nasci}`
 }
