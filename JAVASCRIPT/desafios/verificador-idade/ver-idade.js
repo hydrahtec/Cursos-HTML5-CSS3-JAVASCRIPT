f49cc796b8b6acc0af777f9f3070999d6c2c60af
Eliel S <hydrahtec@gmail.com>
2023-08-23 12:24:52 -0300

    Update js
    Update 2308-1224

--- JAVASCRIPT/desafios/verificador-idade/ver-idade.js
+++ JAVASCRIPT/desafios/verificador-idade/ver-idade.js
@@ -1,7 +1,14 @@
 function idade() {
-    var fnasci=window.document.getElementById('nasc')   
-    var nasci=Number(fnasci.value)
+    var data= new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.getElementById('res')
 
-    var resu=window.document.getElementById('res')
-    resu.innerText=`Seu ano de nascimento é ${nasci}`
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    } else {
+        var radsex=window.document.getElementsByName('radsex')
+        var idade= ano - Number(fano.value)
+        res.innerHTML=`Sua idade é de ${idade} anos`
+    }
 }
