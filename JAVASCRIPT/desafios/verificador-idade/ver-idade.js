d14f5c17ba1b6144f3606b07103a2c480799452d
Eliel S <hydrahtec@gmail.com>
2023-08-26 12:05:15 -0300

    update desafio
    update 2608-1205

--- JAVASCRIPT/desafios/verificador-idade/ver-idade.js
+++ JAVASCRIPT/desafios/verificador-idade/ver-idade.js
@@ -1,11 +1,11 @@
-function idade () {
-    var data= new Date()
+function idade() {
+    var data = new Date()
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
     var res = window.document.getElementById('res')
 
     if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente')
+        window.alert('[Erro] Verifique os dados e tente novamente!')
     } else {
         var fsex = window.document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
@@ -14,7 +14,8 @@ function idade () {
         img.setAttribute('id', 'foto')
 
         if (fsex[0].checked) {
-            genero = 'Homen'
+            genero = 'Homem'
+
             if (idade >= 0 && idade < 10) {
                 //criança
                 img.setAttribute('src', 'crianca-h.png')
@@ -28,13 +29,15 @@ function idade () {
                 // idoso
                 img.setAttribute('src', 'idoso-h.png')
             }
+
         } else if (fsex[1].checked) {
-            genero = 'Mulher'
-            if (idade >=0 && idade < 10) {
-                //crinça
+            genero = 'mulher'
+
+            if (idade >= 0 && idade < 10) {
+                //criança
                 img.setAttribute('src', 'crianca-m.png')
             } else if (idade < 21) {
-                // jovem
+                //jovem
                 img.setAttribute('src', 'joven-m.png')
             } else if (idade < 60) {
                 //adulto
@@ -44,10 +47,10 @@ function idade () {
                 img.setAttribute('src', 'idoso-m.png')
             }
         }
+        res.innerHTML= `Detectamos ${genero} com ${idade} anos!`
         res.style.textAlign= 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos!`   
-        res.appendChild(img)     
-        img.style.display='block'
+        res.appendChild(img)
+        img.style.display= 'block'
         img.style.margin= 'auto'
     }
 }
