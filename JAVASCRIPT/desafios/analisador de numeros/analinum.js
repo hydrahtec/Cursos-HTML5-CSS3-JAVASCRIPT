d58f8d3bd40dfa586d968af88e5c949a03183ca2
Eliel S <hydrahtec@gmail.com>
2023-09-14 09:57:08 -0300

    update

--- JAVASCRIPT/desafios/analisador de numeros/analinum.js
+++ JAVASCRIPT/desafios/analisador de numeros/analinum.js
@@ -3,11 +3,11 @@ function adicionar() {
 let num = document.getElementById('fnum')
 let lista = document.getElementById('flista')
 let res = document.getElementById('res')
-let valores= []
+let valores= [1]
 
 
     function isnumero(n) {
-        if (Number(n)>0 && Number(n)<=100) {
+        if (Number(n) >0 && Number(n) <= 100) {
             return 'true'
         } else {
             return 'false'
@@ -15,14 +15,14 @@ let valores= []
     }
 
     function inlista(n,l) {
-        if (l.indexOf(n) != -1) {
+        if (l.indexOf(Number(n)) != -1) {
             return 'true'
         } else {
             return 'false'
         }
     }
 
-    if (isnumero(num.value) && !inlista(num.value,valores)) {
+    if (isnumero(num.value) && !inlista(num.value, valores)) {
         window.alert('Tudo Ok!!')
     } else {
         window.alert('Erro, o numero que você escolheu não pode ser aceito ou já esta na lista!, verifique e tente novamente.')
