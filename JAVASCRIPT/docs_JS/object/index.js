d9c555a474f2c9259a96e0591579d3d6335b06f9
Eliel Silva <hydrahtec@gmail.com>
2024-04-13 13:22:49 -0300

    docs: estrutura de dados objeto

--- JAVASCRIPT/docs_JS/object/index.js
+++ JAVASCRIPT/docs_JS/object/index.js
@@ -1,1 +1,21 @@
-export {};
+// chave / valor
+
+let produto = {
+    id: 9,
+    nome: 'Cafeteira Elétrica',
+    valor: 99.00
+}
+
+console.log(produto.id) // 9
+console.log(produto.nome) // Cafeteira Elétrica
+console.log(produto.valor) // 99.00
+
+let cliente = {
+    id: 40,
+    nome: 'Jorge Silva',
+    telefone: '(21) 99999-9999'
+}
+
+console.log(cliente.id) // 40
+console.log(cliente.nome) // Jorge Silva
+console.log(cliente.telefone) // (21) 99999-9999
